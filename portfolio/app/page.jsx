8d42245b44c33a4518fb8ef55e491021b7577de0
shahriar2880 <shahriarreza436@gmail.com>
2024-12-16 01:37:17 +0600

    image in header section

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>Home Page</main>
+    <main className="container mx-auto">Home Page</main>
   );
 }
--- portfolio/components/Header.jsx
+++ portfolio/components/Header.jsx
@@ -1,9 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <header>
-      header
+    <header className='py-8 xl:py-12 text-white'>
+      <div className='container mx-auto'>
+        <Link href="/">
+        <h1 className='text-4xl font-semibold'>
+            Shahriar<span className='text-accent'>..</span>
+        </h1>
+        </Link>
+      </div>
     </header>
   )
 }
