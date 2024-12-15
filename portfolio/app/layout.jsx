51d0986a114f3e2e4da127d442a3dd33f33fbf19
shahriar2880 <shahriarreza436@gmail.com>
2024-12-15 21:20:13 +0600

    second commit

--- portfolio/app/layout.jsx
+++ portfolio/app/layout.jsx
@@ -1,6 +1,9 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
+//components
+import Header from "@/components/Header";
+
 const JetBrainsMono = JetBrains_Mono({subsets: ["latin"], weight: ["100" , "200" , "300" , "400" , "500" , "600" , "700" , "800" ], variable: "--font-jetbrainsMono"})
 export default function RootLayout({ children }) {
   return (
@@ -8,6 +11,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${JetBrainsMono.variable} antialiased`}
       >
+        <Header/>
         {children}
       </body>
     </html>
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -22,6 +22,11 @@ export default {
 	},
   	extend: {
   		colors: {
+			primary: '#1c1c22',
+			accent: {
+				DEFAULT: "#00ff99",
+				hover: "#00e187",
+			},
   			background: 'var(--background)',
   			foreground: 'var(--foreground)'
   		},
