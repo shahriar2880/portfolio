2d3d16383c253afc386ca397785e20ad55d332c9
shahriar2880 <shahriarreza436@gmail.com>
2024-12-16 19:48:13 +0600

    create all remaining page

---
+++ portfolio/app/resume/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <div className='container mx-auto'>
+      Resume
+    </div>
+  )
+}
+
+export default Resume
---
+++ portfolio/app/work/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div className='container mx-auto'>
+      Work
+    </div>
+  )
+}
+
+export default Work
