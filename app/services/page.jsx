f861b0a1d1addb4174421b02c90408dba4132da5
shahriar2880 <shahriarreza436@gmail.com>
2024-12-19 17:23:56 +0600

    declare service array

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,36 +1,39 @@
-"use client"
-import {} from 'react'
+"use client";
+import {} from "react";
 
 const services = [
   {
-      num: "01",
-      title: "Web Development",
-      description: "I specialize in creating stunning, user-friendly websites that captivate audiences and drive results. From sleek designs to robust functionalities, I deliver top-notch web solutions tailored to your unique needs. Let's build your online presence together."
+    num: "01",
+    title: "Web Development",
+    description:
+      "I specialize in creating stunning, user-friendly websites that captivate audiences and drive results. From sleek designs to robust functionalities, I deliver top-notch web solutions tailored to your unique needs. Let's build your online presence together.",
   },
   {
-      num: "02",
-      title: "UI/UX Design",
-      description: "I provide expert UI/UX design services to transform your ideas into visually appealing and intuitive digital experiences. From wireframing to prototyping, I ensure seamless user journeys and stunning aesthetics that leave a lasting impression. Let's create designs that captivate and convert."
+    num: "02",
+    title: "UI/UX Design",
+    description:
+      "I provide expert UI/UX design services to transform your ideas into visually appealing and intuitive digital experiences. From wireframing to prototyping, I ensure seamless user journeys and stunning aesthetics that leave a lasting impression. Let's create designs that captivate and convert.",
   },
   {
-      num: "03",
-      title: "Wordpress Theme Customization",
-      description: "Enhance your WordPress website's look and feel with my expert customization services. From tweaking styles and layouts to adding new features, I'll transform your site into a unique online presence. Let's elevate your digital brand."
+    num: "03",
+    title: "Wordpress Theme Customization",
+    description:
+      "Enhance your WordPress website's look and feel with my expert customization services. From tweaking styles and layouts to adding new features, I'll transform your site into a unique online presence. Let's elevate your digital brand.",
   },
   {
-      num: "04",
-      title: "Data Entry",
-      description: "I offer precise and efficient data entry services to streamline your operations. With a keen eye for detail and a commitment to accuracy, I'll handle your data with care. Let me take the burden off your shoulders and ensure your data is organized and accessible."
+    num: "04",
+    title: "Data Entry",
+    description:
+      "I offer precise and efficient data entry services to streamline your operations. With a keen eye for detail and a commitment to accuracy, I'll handle your data with care. Let me take the burden off your shoulders and ensure your data is organized and accessible.",
   },
 ];
 
-
 const Services = () => {
   return (
-    <div className='container mx-auto'>
-      services page
-    </div>
-  )
-}
+    <section>
+      <div className="container mx-auto">services page</div>
+    </section>
+  );
+};
 
-export default Services
+export default Services;
