3b26f2abe20f0f7431a910981268d11b932a3b5a
shahriar2880 <shahriarreza436@gmail.com>
2024-12-24 14:41:13 +0600

    define a object name about

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -3,6 +3,7 @@
 import {FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs} from "react-icons/fa"
 import {SiTailwindcss, SiNextdotjs} from "react-icons/si"
 
+//about data
 const about = {
   title: 'About Me',
   description: "I'm a highly motivated individual driven to build a successful career in [Your Industry]. I'm constantly seeking new challenges and opportunities to learn and grow. I believe in the power of hard work, dedication, and continuous learning.",
@@ -38,6 +39,27 @@ const about = {
   ]
 }
 
+
+//experience data
+ const experience = {
+  icon: '/images/verify.png',
+  title: "My Experience",
+  description: "I am a skilled web developer with a strong foundation in Next.Js, ReactJs, and other modern technologies. I possess excellent problem-solving and communication skills, enabling me to effectively collaborate with teams and deliver high-quality projects.",
+  items: [
+    {
+      company:"Tech Solution Inc.",
+      position:"Front End Developer - Intern",
+      duration:"Fall-2023",
+    },
+    {
+      company:"Web Design Stdio",
+      position:"Web Developer",
+      duration:"2023-Present",
+    },
+  ]
+
+ }
+ 
 const Resume = () => {
   return (
     <section className="">
