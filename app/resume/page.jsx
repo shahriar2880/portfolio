fa9a89e42c3bb8504c96a0a2deeaf744a668e627
shahriar2880 <shahriarreza436@gmail.com>
2024-12-24 12:46:16 +0600

    import from  react-icons/si

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import {FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs} from "react-icons/fa"
-
+import {SiTailwindcss, SiNextdotjs} from "react-icons/si"
 const Resume = () => {
   return (
     <section className="">
