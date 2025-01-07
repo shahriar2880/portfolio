66925f58ac2ee99decf79a164961f09bf524a321
shahriar2880 <shahriarreza436@gmail.com>
2025-01-07 22:45:46 +0600

    outlune number and project description

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -67,8 +67,16 @@ const Work = () => {
     <div className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-          <div>text</div>
-          <div>slider</div>
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div>
+              {/* outline num */}
+              <div className="text-8xl leading-none font-extrabold text-accent">{project.num}</div>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category}</h2>
+              {/* project decription */}
+              <p className="text-white/60">{project.description}</p>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%] ">slider</div>
         </div>
       </div>
     </div>
