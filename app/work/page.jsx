5b00cd1529b5ef8be8c2bd3b5a04bbd591770496
shahriar2880 <shahriarreza436@gmail.com>
2025-01-10 00:02:18 +0600

    live project button

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -94,11 +94,12 @@ const Work = () => {
               <div className="border border-white/20 "></div>
               {/* buttons */}
               <div>
-                <Link href={project.github}>
-                  <TooltipProvider>
+                {/* live project button */}
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
                     <Tooltip>
-                      <TooltipTrigger>
-                        <BsArrowUpRight/>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent"/>
                       </TooltipTrigger>
                       <TooltipContent>
                         <p>Live Project</p>
