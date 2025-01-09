38406b6f8e305784fa677d9d6e125c8031b4ee55
shahriar2880 <shahriarreza436@gmail.com>
2025-01-10 00:36:34 +0600

    github repository button

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -107,6 +107,19 @@ const Work = () => {
                     </Tooltip>
                   </TooltipProvider>
                 </Link>
+                {/* github project button */}
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent"/>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github Repository Link</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
               </div>
             </div>
           </div>
