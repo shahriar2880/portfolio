9fa5353505a62e6da1348f7e52baa547d002514f
shahriar2880 <shahriarreza436@gmail.com>
2025-01-12 00:01:45 +0600

    complete WorkSliderBtns

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -1,8 +1,18 @@
 "use client"
-const WorkSliderBtns = () => {
+
+import { useSwiper } from "swiper/react"
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi"
+
+const WorkSliderBtns = ({containerStyle,btnStyle,iconStyle}) => {
+  const swiper = useSwiper();
   return (
-    <div>
-      
+    <div className="containerStyle">
+      <button className="btnStyle" onClick={()=> swiper.slidePrev()}>
+        <PiCaretLeftBold className="iconStyle"/>
+      </button>
+      <button  className="btnStyle" onClick={()=> swiper.slidePrev()}>
+        <PiCaretRightBold className="iconStyle"/>
+      </button>
     </div>
   )
 }
