bf66537c270fa24c2c73f7dcc8a8ae532a6b3379
shahriar2880 <shahriarreza436@gmail.com>
2025-01-18 20:30:18 +0600

    contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+"use client";
+import {Button} from "@/components/ui/button"
+import {Input} from "@/components/ui/input"
+import {Textarea} from "@/components/ui/textarea"
 
+import {Select, SelectContent, SelectGroup,SelectItem, SelectLabel, SelectTrigger, SelectValue} from "@/components/ui/select"
+import {FaPhoneAlt, FaEnvelope, FaMapMarkerAlt} from "react-icons/fa"
+
+const info = [
+  {
+    icon: <FaPhoneAlt/>,
+    title:'Phone',
+    description: "(+880) 1608 456891"
+  },
+  {
+    icon: <FaEnvelope/>,
+    title:'Email',
+    description: "shahriarreza436@gmail.com"
+  },
+  {
+    icon: <FaMapMarkerAlt/>,
+    title:'Address',
+    description: "6/E, Bash Bari , Mohammadpur"
+  },
+]
 const Contact = () => {
   return (
-    <div className='container mx-auto'>
-      Contact
-    </div>
-  )
-}
+    <section className="py-6">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form>form</form>
+          </div>
+          {/* info */}
+          <div>info</div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
