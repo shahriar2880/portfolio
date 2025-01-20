3c584fba3e2d99c7e7532ef9b817fde77164e92a
shahriar2880 <shahriarreza436@gmail.com>
2025-01-20 15:55:36 +0600

    contact info

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,28 +1,36 @@
 "use client";
-import {Button} from "@/components/ui/button"
-import {Input} from "@/components/ui/input"
-import {Textarea} from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
-import {Select, SelectContent, SelectGroup,SelectItem, SelectLabel, SelectTrigger, SelectValue} from "@/components/ui/select"
-import {FaPhoneAlt, FaEnvelope, FaMapMarkerAlt} from "react-icons/fa"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
 const info = [
   {
-    icon: <FaPhoneAlt/>,
-    title:'Phone',
-    description: "(+880) 1608 456891"
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+880) 1608 456891",
   },
   {
-    icon: <FaEnvelope/>,
-    title:'Email',
-    description: "shahriarreza436@gmail.com"
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "shahriarreza436@gmail.com",
   },
   {
-    icon: <FaMapMarkerAlt/>,
-    title:'Address',
-    description: "6/E, Bash Bari , Mohammadpur"
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "6/E, Bash Bari , Mohammadpur",
   },
-]
+];
 const Contact = () => {
   return (
     <section className="py-6">
@@ -31,19 +39,22 @@ const Contact = () => {
           {/* form */}
           <div className="xl:h-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent text-center">Let's work together</h3>
+              <h3 className="text-4xl text-accent text-center">
+                Let's work together
+              </h3>
               <p className="text-white/60">lorem10</p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type='firstname' placeholder="First Name"/>
-                <Input type='lastname' placeholder="Last Name"/>
-                <Input type='email' placeholder="Email Address"/>
-                <Input type='phone' placeholder="Phone Number"/>
+                <Input type="firstname" placeholder="First Name" />
+                <Input type="lastname" placeholder="Last Name" />
+                <Input type="email" placeholder="Email Address" />
+                <Input type="phone" placeholder="Phone Number" />
               </div>
               {/* select */}
               <Select>
                 <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Select a service"/></SelectTrigger>
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
                     <SelectLabel>Select a service</SelectLabel>
@@ -54,13 +65,34 @@ const Contact = () => {
                 </SelectContent>
               </Select>
               {/* text area */}
-              <Textarea className="h-[200px]" placeholder="Type your message here..."/>
+              <Textarea
+                className="h-[200px]"
+                placeholder="Type your message here..."
+              />
               {/* btn */}
-              <Button size="md" className="max-w-40 h-12">Send Message</Button>
+              <Button size="md" className="max-w-40 h-12">
+                Send Message
+              </Button>
             </form>
           </div>
           {/* info */}
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">info</div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className=" w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </section>
