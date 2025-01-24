b84bc7bc90b60b318c48ba98a97b9e22ae8ebf3b
shahriar2880 <shahriarreza436@gmail.com>
2025-01-24 23:44:52 +0600

    create arrow function

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -42,7 +42,7 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" onSubmit={sendEmail}>
               <h3 className="text-4xl text-accent text-center">
                 Let's work together
               </h3>
