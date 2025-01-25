93a10dc63434651ffc00434526e907c32593a199
shahriar2880 <shahriarreza436@gmail.com>
2025-01-25 20:39:33 +0600

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,16 +4,17 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import emailjs from '@emailjs/browser';
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+// import {
+//   Select,
+//   SelectContent,
+//   SelectGroup,
+//   SelectItem,
+//   SelectLabel,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/ui/select";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { useRef } from "react";
 
 
 const info = [
@@ -37,16 +38,18 @@ const info = [
 const Contact = () => {
   const form = useRef();
 
+
   const sendEmail = (e) => {
     e.preventDefault();
 
+
     emailjs
-      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
-        publicKey: 'YOUR_PUBLIC_KEY',
+      .sendForm('service_rea5kkm', 'template_bukw9zg', form.current, {
+        publicKey: 'LGWmw9axB6GPWbQxq',
       })
       .then(
         () => {
-          console.log('SUCCESS!');
+          form.current.reset();
         },
         (error) => {
           console.log('FAILED...', error.text);
@@ -54,45 +57,47 @@ const Contact = () => {
       );
   };
 
+
   return (
     <section className="py-6">
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" onSubmit={sendEmail}>
+            <form ref={form} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" onSubmit={sendEmail}>
               <h3 className="text-4xl text-accent text-center">
                 Let's work together
               </h3>
               <p className="text-white/60">lorem10</p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="First Name" />
-                <Input type="lastname" placeholder="Last Name" />
-                <Input type="email" placeholder="Email Address" />
-                <Input type="phone" placeholder="Phone Number" />
+                <Input type="firstname" placeholder="First Name" name="firstname"/>
+                <Input type="lastname" placeholder="Last Name" name="lastname"/>
+                <Input type="email" placeholder="Email Address" name="user_email" />
+                <Input type="phone" placeholder="Phone Number" name="phone" />
               </div>
               {/* select */}
-              <Select>
+              {/* <Select>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Select a service" />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
                     <SelectLabel>Select a service</SelectLabel>
-                    <SelectItem value="est">Web Development</SelectItem>
-                    <SelectItem value="cst">UI/UX Design</SelectItem>
-                    <SelectItem value="mst">Logo Design</SelectItem>
+                    <SelectItem name="select"  value="est">Web Development</SelectItem>
+                    <SelectItem name="select"  value="cst">UI/UX Design</SelectItem>
+                    <SelectItem name="select"  value="mst">Logo Design</SelectItem>
                   </SelectGroup>
                 </SelectContent>
-              </Select>
+              </Select> */}
               {/* text area */}
               <Textarea
                 className="h-[200px]"
                 placeholder="Type your message here..."
+                name="message"
               />
               {/* btn */}
-              <Button size="md" className="max-w-40 h-12">
+              <Button size="md" className="max-w-40 h-12" value="Send">
                 Send Message
               </Button>
             </form>
